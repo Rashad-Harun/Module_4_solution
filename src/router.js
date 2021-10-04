81ef0d46d18516101d7452f24de0376af35f0888
Rashad-Harun <Rashad734@hotmail.com>
2021-10-04 15:43:08 +0200

    renamed

--- src/router.js
+++ src/router.js
@@ -30,7 +30,7 @@
         
         .state('items', {
             url: '/items/:id',
-            templateUrl: '/views/itemslist.html',
+            templateUrl: '/views/items.html',
             controller: 'ItemsController as ctrl',
             resolve: {
                 items: ['MenuDataService', '$stateParams', function(MenuDataService, $stateParams){
